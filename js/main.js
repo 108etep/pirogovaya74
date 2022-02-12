5a88697ddefeaa369d51622b4272812978b3fd32
Yuzhnii <70344381+108etep@users.noreply.github.com>
2022-02-12 23:12:36 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+$('.slider-pie').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  arrows:true,
+  prevArrow:'<button type="button" class="slick-prev"><img src="img/arrow.png" alt=""></button>',
+  nextArrow:'<button type="button" class="slick-next"><img src="img/arrow.png" alt=""></button>'
+
+
+  });
+
+
+  
+});
